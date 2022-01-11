03d6b736572f38032ea22bf28b7d3ab7e9fd7585
Jeongsoo Kim <jeongsk7@uci.edu>
2022-01-10 23:12:10 -0800

    Refactoring Flutter Widgets practice

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEb1555);
+const bottomContainerTopMargin = 10.0;
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,15 +17,61 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      clr: activeCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      clr: activeCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: ReusableCard(
+              clr: activeCardColor,
+            )),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    clr: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    clr: activeCardColor,
+                  ),
+                ),
+              ],
+            )),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: bottomContainerTopMargin),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            ),
+          ],
+        ));
   }
 }
